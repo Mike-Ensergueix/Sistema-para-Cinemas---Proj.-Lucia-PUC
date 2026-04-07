e24ca9a61c16aa5a5b17a3c476c789b7cb5a8078
Mike Ensergueix <mike.ensergueix@gmail.com>
2026-04-07 16:29:49 -0300

    Aprimorei a funcionalidade do menu com validação de entrada e comentários.
    Foi adicionada validação de entrada e comentários à função de menu.

--- Projeto.c
+++ Projeto.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void menu()
+void menu()           //Criacao do menu
 {
-    int escolha = -1;
+    int escolha = -1; //variavel para escolha
 
     do
     {
+    /*                impressao do menu                        */
         printf("\n\t-----Sistema de Controle de Assentos------");
         printf("\n\t|  1. Exibir mapa da sala                |");
         printf("\n\t|  2. Vender ingresso manualmente        |");
@@ -17,40 +18,48 @@ void menu()
         printf("\n\t|  0. Sair                               |");
         printf("\n\t------------------------------------------");
         printf("\n\n\tDigite para Escolher: ");
-        scanf("%d", &escolha);
 
+        escolha = -1;               //reset para evitar erros de opcao invalida
+        scanf("%d", &escolha);      //escolha da opcao
+        while (getchar() != '\n');  //limpeza de characteres invalidos (evita loop infinito)
 
         switch(escolha)
         {
             case 1:
                 printf("\n\t1. Exibindo mapa da sala");
+                //funcao mostrar mapa
                 break;
             case 2:
                 printf("\n\t2. Venda de ingresso manual");
+                //funcao venda manaul
                 break;
             case 3:
                 printf("\n\t3. Cancelando venda");
+                //funcao cancelar venda
                 break;
             case 4:
                 printf("\n\t4. Sugerindo melhor assento disponivel");
+                //funcao sugerur melhor assento
                 break;
             case 5:
                 printf("\n\t5. Verificando fileiras criticas");
+                //funcao verificar fileiras
                 break;
             case 6:
                 printf("\n\t6. Mostrar estatisticas gerais");
+                //funcao mostrar estatisticas
                 break;
             case 0:
-                printf("\n\tSaindo...");
+                printf("\n\tSaindo...");  //escolha = 0 encerra o programa
                 break;
             default:
-                printf("\n\tOpcao Invalida!");
+                printf("\n\tOpcao Invalida!"); //escolha != 0 a 6 opcao invalida
                 break;
         }
     } while (escolha != 0);
 }
 int main()
 {
-    menu();
+    menu();   //chama o menu
     return 0;
 }
